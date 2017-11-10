2aeaf3fed39acd13756b73acfa8ad3f6fc3978e3
wesdriver45 <wesdriver45@gmail.com>
2017-11-09 21:03:05 -0500

    changes

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// <script src="https://www.gstatic.com/firebasejs/4.6.2/firebase.js"></script>
+// <script>
+//   // Initialize Firebase
+//   var config = {
+//     apiKey: "AIzaSyCLL3aCBdebJC_74YWY04BLw48vJPBL1kM",
+//     authDomain: "decision-dictator.firebaseapp.com",
+//     databaseURL: "https://decision-dictator.firebaseio.com",
+//     projectId: "decision-dictator",
+//     storageBucket: "",
+//     messagingSenderId: "1029892439948"
+//   };
+//   firebase.initializeApp(config);
+// </script>
