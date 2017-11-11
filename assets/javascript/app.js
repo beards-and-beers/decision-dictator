d74ff974f74bd909686b21694ac17be18a340332
Connor Leach <leachc820@gmail.com>
2017-11-11 12:46:37 -0500

    javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,3 +11,30 @@
 //   };
 //   firebase.initializeApp(config);
 // </script>
+
+var APIKey_brewery = "aa416dc9ba758638ac327b58ee7ee727";
+
+var queryURL_brewery = "http://api.brewerydb.com/v2/?key=" + APIKey_brewery;
+
+var APIKey_zomato = "262a8901410f92714e29cd56d11b11bf";
+
+var queryURL_zomato = "https://developers.zomato.com/api/v2.1/search?entity_id=95008&entity_type=zone&start=0&count=10";
+
+
+	$.ajax({
+		url: queryURL_brewery,
+		method: "GET"
+	}).done(function(response) {
+		console.log(queryURL_brewery);
+	});
+
+	$.ajax({
+		type: "POST",
+		url: queryURL_zomato,
+		headers: {
+			"key": "262a8901410f92714e29cd56d11b11bf"
+		}
+	}).done(function(data) {
+		console.log(data);
+	});
+
