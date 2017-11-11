4f15e001ace62a3a7fb8efe63a6e5b111ac1aa27
Scott Bower <scottbower@zaphod.local>
2017-11-11 15:01:22 -0500

    working zomato

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,12 @@
+
+// APP STRUCTURE
+
+//CODE
+//
+
+$(document).ready(function() {
+
+//////// FIREBASE ///////
 // <script src="https://www.gstatic.com/firebasejs/4.6.2/firebase.js"></script>
 // <script>
 //   // Initialize Firebase
@@ -12,29 +21,67 @@
 //   firebase.initializeApp(config);
 // </script>
 
-var APIKey_brewery = "aa416dc9ba758638ac327b58ee7ee727";
 
-var queryURL_brewery = "http://api.brewerydb.com/v2/?key=" + APIKey_brewery;
-
-var APIKey_zomato = "262a8901410f92714e29cd56d11b11bf";
-
-var queryURL_zomato = "https://developers.zomato.com/api/v2.1/search?entity_id=95008&entity_type=zone&start=0&count=10";
+//////// GLOBAL STUFF ///////
+	
 
 
-	$.ajax({
-		url: queryURL_brewery,
-		method: "GET"
-	}).done(function(response) {
-		console.log(queryURL_brewery);
-	});
 
-	$.ajax({
-		type: "POST",
-		url: queryURL_zomato,
-		headers: {
-			"key": "262a8901410f92714e29cd56d11b11bf"
+
+
+
+
+
+
+//////// METHODS ///////
+		var app = {
+			setup: function() {
+			// hide sections of app not using
+			},
+
+			results_brewery: function() {
+				var APIKey_brewery = "aa416dc9ba758638ac327b58ee7ee727";
+				var queryURL_brewery = "http://api.brewerydb.com/v2/?key=" + APIKey_brewery;
+
+				$.ajax({
+					url: queryURL_brewery,
+					method: "GET"
+					}).done(function(response) {
+						console.log(queryURL_brewery);
+				});
+			},
+
+			results_zomato: function() {
+				var APIKey_zomato = "262a8901410f92714e29cd56d11b11bf";
+				var queryURL_zomato = "https://developers.zomato.com/api/v2.1/search?entity_id=95008&entity_type=zone&start=10&count=1";
+
+
+				$.ajax({
+					url: queryURL_zomato,
+					method: "GET",
+					headers: {
+						"user-key": "262a8901410f92714e29cd56d11b11bf"
+					}
+					}).done(function(data) {
+						console.log(data);
+				});
+			}
+
 		}
-	}).done(function(data) {
-		console.log(data);
-	});
 
+////// RUN APP //////
+		app.setup();
+
+
+//// LISTENERS FOR page_submitData_Region //// 
+		$(document).on("click", "#btn_submitCard", function() {
+			app.results_zomato();
+
+			// if ( $(this).attr("data-value")==="1.1" && $(this).attr("data-value")==="2.1"  ) {
+			// 	app.results_zomato();
+			// };
+		
+
+		});
+
+});
